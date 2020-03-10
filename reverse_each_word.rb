598cd2dd408183af235fdbfeec1de4589353c057
Brandon Marrero <brandonmar97@gmail.com>
2020-03-10 22:20:42 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,8 @@
 def reverse_each_word(sentence)
   words_array = sentence.split(" ")
+  reverse_array = []
   words_array.collect do |word|
-    word.reverse
+    reverse_array << word.reverse
   end
-  words_array.join(" ")
+  reverse_array.join(" ")
 end
